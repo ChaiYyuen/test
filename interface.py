419f7530165a6d8ba4788d441007d6bf538132ea
ChaiYyuen <chaiyauyuan@gmail.com>
2024-10-17 18:27:08 +0000

    1

--- interface.py
+++ interface.py
@@ -21,7 +21,7 @@ def login_page(auth_url):
       unsafe_allow_html=True)
   st.markdown('<div class="divider"></div>', unsafe_allow_html=True)
   logo = "/pic/Spotify.png"
-  render_image("/pic/Spotify.png")
+  render_image("pic/Spotify.png")
   left_co, cent_co, last_co = st.columns(3)
   with cent_co:
     st.image(logo)
