16a7aeebd473705c60722231f51f36370291725e
ChaiYyuen <chaiyauyuan@gmail.com>
2024-10-17 21:44:57 +0000

    1

--- interface.py
+++ interface.py
@@ -204,7 +204,7 @@ def success_page():
   playlists_data = st.session_state['playlists']
   playlist_items = playlists_data['items']
   playlist_id = [id['id'] for id in playlist_items]
-  songs = func.get_user_playlists_items(token, playlist_id[0])
+  songs = func.get_user_playlists_items(token)
   st.write(songs)
   
   st.write(playlist_id)
