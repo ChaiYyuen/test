5e099668fb0e98bafb285bd6def5a4a0b1d5b541
ChaiYyuen <chaiyauyuan@gmail.com>
2024-10-17 04:09:08 +0000

    1

--- Server.py
+++ Server.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta
 # Spotify API credentials
 CLIENT_ID = st.secrets["CLIENT_ID"]
 CLIENT_SECRET = st.secrets["CLIENT_SECRET"]
-REDIRECT_URI = "https://genresync.streamlit.app/"  # Update this to your Streamlit app URL
+REDIRECT_URI = "https://genresync.streamlit.app/callback"  # Update this to your Streamlit app URL
 
 AUTH_URL = "https://accounts.spotify.com/authorize"
 TOKEN_URL = "https://accounts.spotify.com/api/token"
