35546ad01b4cfafb4a7b42535f34e889605473a5
ChaiYyuen <chaiyauyuan@gmail.com>
2024-10-17 23:18:01 +0000

    1

--- functions.py
+++ functions.py
@@ -41,7 +41,7 @@ def get_user_playlists_items(token, id):
   json_result = json.loads(result.content)
   if len(json_result) == 0:
     return None
-  return result
+  return json_result
 
 
 def search_for_artist(token, artist_name):
