ab476ca8f7b420e6a5ce05c8ffb6c542034c4b1b
ChaiYyuen <chaiyauyuan@gmail.com>
2024-10-17 18:47:55 +0000

    1

--- interface.py
+++ interface.py
@@ -105,10 +105,10 @@ def login_page(auth_url):
       "<div class='title'>GenreSync: Tune in to Musical Diversity</div>",
       unsafe_allow_html=True)
   st.markdown('<div class="divider"></div>', unsafe_allow_html=True)
-  left_co, cent_co, last_co = st.columns(3)
+  left_co, cent_co, last_co = st.columns([1, 1, 1])
   with cent_co:
     render_image("pic/Spotify.png")
   st.markdown('<p class="subtitle">Log in to your Spotify account!</p>',
               unsafe_allow_html=True)
   # Spotify Login Button (Mock login functionality)
-  st.link_button('Log in with Spotify', auth_url)
+  cent_co.link_button('Log in with Spotify', auth_url)
