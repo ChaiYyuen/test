11b982b971e7b61a67a65d40ddc86c09c193f96e
ChaiYyuen <chaiyauyuan@gmail.com>
2024-10-17 21:02:41 +0000

    1

--- interface.py
+++ interface.py
@@ -202,9 +202,10 @@ def success_page():
   playlists_data = st.session_state['playlists']
   playlist_items = playlists_data['items']
   playlist_name = []
+  playlist_photo = []
   for name in playlist_items:
     playlist_name.append(name['name'])
-  st.write(playlist_name)
+  st.write(playlist_items)
   # Extract playlist and song data
   # playlist_name = data["user_playlist"]["playlist_name"]
   # songs = data["user_playlist"]["songs"]
